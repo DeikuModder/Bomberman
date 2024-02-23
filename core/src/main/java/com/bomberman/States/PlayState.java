dc69a3c76f51980887716fd9cb8446731414daf7
Gab <gabrieltc555@gmail.com>
2024-02-23 18:20:39 -0400

    Create interface for states, and playState

---
+++ core/src/main/java/com/bomberman/States/PlayState.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bomberman.States;
+
+/**
+ *
+ * @author gabri
+ */
+public class PlayState implements statesInterface {
+    
+    public void create() {
+        
+    }
+    
+    public void render() {
+        
+    }
+    
+    public void dispose() {
+        
+    }
+}
