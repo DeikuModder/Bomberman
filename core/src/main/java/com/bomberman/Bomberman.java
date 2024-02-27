b48884d208efeae76c47094fbbd8d30667c42d2d
Gab <gabrieltc555@gmail.com>
2024-02-27 12:16:52 -0400

    Change background color

--- core/src/main/java/com/bomberman/Bomberman.java
+++ core/src/main/java/com/bomberman/Bomberman.java
@@ -24,7 +24,7 @@ public class Bomberman extends ApplicationAdapter {
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0.2f, 0.7f, 0.2f, 1f);
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
         playState.render();
