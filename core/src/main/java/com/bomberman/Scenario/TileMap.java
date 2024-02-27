3efeaeec6e9a6c315e2428660cdcf1cc6e2a6c22
Gab <gabrieltc555@gmail.com>
2024-02-27 12:19:03 -0400

    Render empty and wall blocks on screen, add isCollidable method

--- core/src/main/java/com/bomberman/Scenario/TileMap.java
+++ core/src/main/java/com/bomberman/Scenario/TileMap.java
@@ -6,7 +6,6 @@ package com.bomberman.Scenario;
 
 import com.bomberman.ConstantValues;
 import com.bomberman.Entities.Block;
-import java.util.ArrayList;
 
 /**
  *
@@ -16,26 +15,112 @@ public class TileMap {
     private final ConstantValues constantValues = new ConstantValues();
     private final int amountOfXBlocks = constantValues.WINDOW_WIDTH / constantValues.BLOCK_SIZE;
     private final int amountOfYBlocks = constantValues.WINDOW_HEIGHT / constantValues.BLOCK_SIZE;
-    private Row[] gridBlockArr;
+    private final Row[] gridBlockArr;
+    private final String spriteBlockTexture = "blockx32.png";
+    private final String spriteEmptyTexture = "groundx32.png";
     
     public TileMap() {
-        int yOrigin = 0;
+        int yTileOrigin = 0;
+        int xTileOrigin = 0;
         gridBlockArr = new Row[amountOfYBlocks];
         
+        //first fill the map with collidable blocks
         for (int i = 0; i < amountOfYBlocks; i++){
-            Row gridRow = new Row(amountOfXBlocks, 0, yOrigin, "grid_block.png", false);
-            gridBlockArr[i] = gridRow;
-            yOrigin += 64;
+            gridBlockArr[i] = new Row(amountOfXBlocks, xTileOrigin, yTileOrigin, spriteBlockTexture, true);
+            yTileOrigin += constantValues.BLOCK_SIZE;
         }
         
-        System.out.println(gridBlockArr[8].tiles[0].x);
-        System.out.println(gridBlockArr[8].tiles[0].y);
+        //reset y and x origin back to 0 after each loop
+        
+        yTileOrigin = 0;
+        xTileOrigin = 0;
+        
+        //then create the empty spaces
+        
+        //empty down path
+        for (int i = 1; i < amountOfXBlocks - 1; i++){
+            int x = gridBlockArr[1].tiles[i].x;
+            int y = gridBlockArr[1].tiles[i].y;
+            
+            gridBlockArr[1].tiles[i] = new Block(spriteEmptyTexture, x, y, false);
+            x += constantValues.BLOCK_SIZE;
+        }
+        
+        //empty upper path
+        for (int i = 1; i < amountOfXBlocks - 1; i++){
+            int x = gridBlockArr[amountOfYBlocks - 2].tiles[i].x;
+            int y = gridBlockArr[amountOfYBlocks - 2].tiles[i].y;
+            
+            gridBlockArr[amountOfYBlocks - 2].tiles[i] = new Block(spriteEmptyTexture, x, y, false);
+            x += constantValues.BLOCK_SIZE;
+        }
+        
+        //fill the inner empty space
+        for (int i = 3; i < amountOfYBlocks - 3; i++){
+            for (int j = 3; j < amountOfXBlocks - 3; j++){
+                
+                gridBlockArr[i].tiles[j] = new Block(spriteEmptyTexture, xTileOrigin + (constantValues.BLOCK_SIZE * 3), yTileOrigin + constantValues.BLOCK_SIZE * 3, false);
+                xTileOrigin += constantValues.BLOCK_SIZE;
+            }
+            xTileOrigin = 0;
+            yTileOrigin += constantValues.BLOCK_SIZE;
+        }
+        
+        yTileOrigin = 0;
+        xTileOrigin = 0;
+        
+        //empty left path
+        for (int i = 2; i < amountOfYBlocks - 2; i++){
+            int x = gridBlockArr[i].tiles[1].x;
+            int y = gridBlockArr[i].tiles[1].y;
+            
+            gridBlockArr[i].tiles[1] = new Block(spriteEmptyTexture, x, y, false);
+            y += constantValues.BLOCK_SIZE;
+        }
+     
+        //empty right path
+        for (int i = 2; i < amountOfYBlocks - 2; i++){
+            int x = gridBlockArr[i].tiles[amountOfXBlocks - 2].x;
+            int y = gridBlockArr[i].tiles[amountOfXBlocks - 2].y;
+            
+            gridBlockArr[i].tiles[amountOfXBlocks - 2] = new Block(spriteEmptyTexture, x, y, false);
+            y += constantValues.BLOCK_SIZE;
+        }
+        /*
+        Ejemplo de como acceder a un tile desde aqui:
+        gridBlockArr[0].tiles[0] = El tile con origen 0,0 en coordenadas, siendo en este caso Y, X el orden.
+        */
     }
     
     public void render() {
        for (Row gridRow : gridBlockArr){
            gridRow.render();
        }
+    }
+    
+    /**
+     * 
+     * @param x
+     * Position in X that needs to be checked
+     * @param y
+     * Position in Y that needs to be checked
+     * @return 
+     * True if the tile is Coliidable, false otherwise
+     */
+    public boolean isCollidable(int x, int y) {
+        boolean collidable = false;
+        
+        for (Row gridRow : gridBlockArr){
+            for (Block tile : gridRow.tiles){
+                if (tile.x == x && tile.y == y){
+                    if (tile.isCollidable){
+                        collidable = true;
+                    }
+                }
+            }
+        }
+        
+        return collidable;
     }
     
     public void dispose() {
